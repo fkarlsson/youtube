cc04aaad22ed60fe98bf365610c25480cbc6c9cc
Fredrik Karlsson <fkarlsson@gmail.com>
2011-09-13 10:37:40 +0200

    first commit

---
+++ youtube.pl
@@ -0,0 +1,65 @@
+use strict;
+use vars qw($VERSION %IRSSI);
+
+use LWP::UserAgent;
+
+use Irssi;
+$VERSION = '20100913';
+%IRSSI = (
+	authors     => 'tuqs',
+	contact     => 'tuqs@core.ws',
+	name        => 'youtube',
+	description => 'shows the title and description from the video',
+	license     => 'Public Domain',
+	changed     => $VERSION,
+);
+
+
+#
+# 20081105 - function rewrite
+# 20081226 - fixed regex
+# 20090206 - some further fixes
+# 20100913 - added support for youtu.be links
+#
+# usage:
+# /script load youtube
+# enjoy ;o)
+#
+
+
+sub htmlfix {
+	my $s = shift;
+	$s =~ s!&amp;!&!g;
+	$s =~ s!&quot;!"!g;
+	$s =~ s!&rsquo;!'!g;
+	$s =~ s!&\#039;!'!g;
+	$s =~ s!&ndash;!-!g;
+	$s =~ s!&lt;!<!g;
+	$s =~ s!&gt;!>!g;
+	$s =~ s!</?br\s?/?>! !g;
+	$s =~ s!\(<a.+>more</a>\)!!gi; # remove more
+	$s =~ s!<a.+>.+</a>!!g; # remove links
+	$s =~ s/\s+$//g; # last but not least remove tailing blanks
+	return $s;
+}
+
+sub youtube {
+	my ( $server, $msg, $nick, $addr, $target ) = @_;
+	my $window = $server->window_find_item($target);
+
+	if ($msg =~ /(youtube\.com\/watch\?v=|youtu\.be\/)(.{11})/) {
+		my $ua = LWP::UserAgent->new;
+		my $r = $ua->get("http://m.youtube.com/details?v=$2&warned=1&hl=en&fulldescription=1");
+		Irssi::signal_continue(@_);
+		return unless defined $r;
+
+		# yes i know, parsing html with regular expressions is stupid
+		# but i want to keep the dependencies as minimal as possible
+		$r->content =~ /<title>YouTube\s-\s(.+)<\/title>/;
+		if (defined $1) {
+			$window->command("echo ".chr(3)."0"."YouTube:".chr(3)." ".htmlfix($1));
+		}
+	}
+}
+
+Irssi::signal_add_first('message public', \&youtube);
